dd63d5134a5d7942028381b28aa87ce5287901d1
Dan Oxlade <dan.oxlade@gmail.com>
2010-12-18 16:52:52 +0000

    starting problem3

--- src/test/scala/EulerSpec.scala
+++ src/test/scala/EulerSpec.scala
@@ -2,21 +2,8 @@ import org.specs._
 
 class EulerSpec extends Specification {
   
-  "Euler Problem Number 2" should {
-    "Find the sum of all the even-valued terms in the Fibonacci sequence which do not exceed four million" in {
-		
-		val fourMillion = 4000000
-		
-		val result = fib_iter.collect{  
-								case (a,b) => a
-							}
-							.filter( _ % 2 == 0 )
-							.takeWhile( _ < fourMillion )
-							.foldLeft(0) {
-								case (acc, i) => acc + i
-							}
-		println("Result %s".format(result))
-		result must_== 4613732
+  "Euler Problem Number 3" should {
+    "What is the largest prime factor of the number 600851475143" in {
     }
   }
 
