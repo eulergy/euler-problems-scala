bde8fe986fc89c5557b08083ba2377e0cb5fb8fe
Dan Oxlade <dan.oxlade@gmail.com>
2010-12-19 22:17:42 +0000

    solved problem6

--- src/test/scala/EulerSpec.scala
+++ src/test/scala/EulerSpec.scala
@@ -1,15 +1,17 @@
 import org.specs._
 import eulergy.NumberHelpers._
+import scala.math._
 
 class EulerSpec extends Specification {
 
-  "Euler Problem Number 5" should {
-    "What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20" in {
-		val divisors = (1 to 20)
-		val result = Iterator.from(20).dropWhile {
-			case i => !divisors.forall( _ isDivisorOf i )
-		}.next
+  "Euler Problem Number 6" should {
+    "Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum" in {
+		val sumOfSquares = Iterator.range(1, 101).collect {
+			case i => pow(i, 2)
+		}.sum
+		val squareOfSums = pow(Iterator.range(1, 101).sum, 2)
 		
+		val result = squareOfSums - sumOfSquares
 		println("Result: %s".format(result))
 		result must_== 232792560
     }
