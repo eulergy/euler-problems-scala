e0962ffec0ee13797001de6a4dc2da5bd2ccc437
Dan Oxlade <dan.oxlade@gmail.com>
2010-12-21 06:19:27 -0800

    tidy up indentation

--- src/test/scala/EulerSpec.scala
+++ src/test/scala/EulerSpec.scala
@@ -8,13 +8,12 @@ class EulerSpec extends Specification {
 		val fourMillion = 4000000
 		
 		val result = fib_iter.collect{  
-								case (a,b) => a
-							}
-							.filter( _ % 2 == 0 )
-							.takeWhile( _ < fourMillion )
-							.foldLeft(0) {
-								case (acc, i) => acc + i
-							}
+			case (a,b) => a
+		}.filter( _ % 2 == 0 )
+		.takeWhile( _ < fourMillion )
+		.foldLeft(0) {
+			case (acc, i) => acc + i
+		}
 		println("Result %s".format(result))
 		result must_== 4613732
     }
